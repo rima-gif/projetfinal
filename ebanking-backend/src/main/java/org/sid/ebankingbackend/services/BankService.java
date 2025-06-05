dfd67c46526c4a42d28ff719452d153f590720bf
rima-gif <achourryma971@gmail.com>
2025-06-05 17:30:00 +0200

    a

--- ebanking-backend/src/main/java/org/sid/ebankingbackend/services/BankService.java
+++ ebanking-backend/src/main/java/org/sid/ebankingbackend/services/BankService.java
@@ -5,40 +5,44 @@ import org.sid.ebankingbackend.entities.BankAccount;
 import org.sid.ebankingbackend.entities.CurrentAccount;
 import org.sid.ebankingbackend.entities.SavingAccount;
 import org.sid.ebankingbackend.repositories.BankAccountRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
+
 @Service
-//Avec Transactional LAZY s'execute sans aucun problème
 @Transactional
 public class BankService {
+    private static final Logger logger = LoggerFactory.getLogger(BankService.class);
+
     @Autowired
     private BankAccountRepository bankAccountRepository;
-    public void consulter(){
-        BankAccount bankAccount = bankAccountRepository.findById("0cd0b20c-b765-4cea-90bf-564da85deae8").orElse(null);
-        if(bankAccount!=null){
-            System.out.println("***********************");
-            System.out.println(bankAccount.getId());
-            System.out.println(bankAccount.getBalance());
-            System.out.println(bankAccount.getStatus());
-            System.out.println(bankAccount.getCreateAt());
-            System.out.println(bankAccount.getCustomer().getName());
-            System.out.println(bankAccount.getClass().getSimpleName());
-            if(bankAccount instanceof CurrentAccount){
-                System.out.println("Over Draft : "+((CurrentAccount) bankAccount).getOverDraft());
-            }
-            else if (bankAccount instanceof SavingAccount)
-            {
-                System.out.println("Rate : "+((SavingAccount) bankAccount).getInterestRate());
-            }
-            bankAccount.getAccountOperations().forEach(accountOperation -> {
-                System.out.println("============================");
-                System.out.print(accountOperation.getType()+"\t");
-                System.out.print(accountOperation.getOperationDate()+"\t");
-                System.out.println(accountOperation.getAmount()+"\t");
 
-            });
-            System.out.println("============================");
-            System.out.println("***********************");
+    public void consulter(){
+        bankAccountRepository.findById("0cd0b20c-b765-4cea-90bf-564da85deae8").ifPresent(this::afficherDetailsCompte);
+    }
+
+    private void afficherDetailsCompte(BankAccount bankAccount) {
+        logger.info("***********************");
+        logger.info("ID: {}", bankAccount.getId());
+        logger.info("Balance: {}", bankAccount.getBalance());
+        logger.info("Status: {}", bankAccount.getStatus());
+        logger.info("Created At: {}", bankAccount.getCreateAt());
+        logger.info("Customer: {}", bankAccount.getCustomer().getName());
+        logger.info("Account Type: {}", bankAccount.getClass().getSimpleName());
+
+        if (bankAccount instanceof CurrentAccount currentAccount) {
+            logger.info("Over Draft: {}", currentAccount.getOverDraft());
+        } else if (bankAccount instanceof SavingAccount savingAccount) {
+            logger.info("Rate: {}", savingAccount.getInterestRate());
         }
+
+        bankAccount.getAccountOperations().forEach(op -> {
+            logger.info("Operation: {} {} {}", op.getType(), op.getOperationDate(), op.getAmount());
+        });
+        logger.info("============================");
     }
 }
