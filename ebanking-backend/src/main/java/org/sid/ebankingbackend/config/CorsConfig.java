f979efa2944ac689abe20cf6b6d54f684cb7127a
rima-gif <achourryma971@gmail.com>
2025-06-06 05:44:28 +0200

    Update CorsConfig.java

--- ebanking-backend/src/main/java/org/sid/ebankingbackend/config/CorsConfig.java
+++ ebanking-backend/src/main/java/org/sid/ebankingbackend/config/CorsConfig.java
@@ -1,4 +1,5 @@
 package org.sid.ebankingbackend.config;
+import org.springframework.context.annotation.Bean;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -6,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
+@Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
